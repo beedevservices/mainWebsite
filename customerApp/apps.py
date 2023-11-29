102ff3f90d07263241a0715d1cfeed7cb779ce01
Melissa Longenberger <melissalongenberger24@gmail.com>
2023-11-29 11:05:06 -0500

    pushing changes

--- customerApp/apps.py
+++ customerApp/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class CustomerappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'customerApp'
+    def read(self):
+        import customerApp.signals
