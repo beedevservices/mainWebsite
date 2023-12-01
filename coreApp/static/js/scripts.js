36f97d00cef2d81dcdb5ccab54614d95e4386c0d
melissa-24 <mlongenberger@codingdojo.com>
2023-12-01 18:07:48 -0500

    saving

--- coreApp/static/js/scripts.js
+++ coreApp/static/js/scripts.js
@@ -32,3 +32,13 @@ function auth() {
         t.innerHTML = 'Login'
     }
 }
+
+function menu() {
+    var x = document.getElementById("myLinks")
+    if(x.style.display === 'flex') {
+        x.style.display = none
+    } else {
+        x.style.display = 'flex'
+        x.style.flexDirection = 'column'
+    }
+}
