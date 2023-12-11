99052e31d49cde56711630ed40e39864019b4dd6
melissa-24 <mlongenberger@codingdojo.com>
2023-12-10 20:30:07 -0500

    saving

--- coreApp/models.py
+++ coreApp/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 
+packageTypes = [
+    ('Website Only', 'Creation or Updating of Website'),
+    ('Add ons', 'Items that can be added on to any package'),
+    ('Maintenance Packages', 'Plans for Website maintenance'),
+    ('All inclusive', 'Starting from the ground up creating and maintaining website')
+]
+
 class Service(models.Model):
     title = models.CharField(max_length=255)
     package = models.CharField(max_length=255, blank=True, null=True)
     price = models.CharField(max_length=255, blank=True, null=True)
     quoteNeeded = models.BooleanField(default=0)
+    packageType = models.CharField(max_length=255, choices=packageTypes, default='Website Only')
+    order = models.IntegerField(default=1)
     timeFrame = models.CharField(max_length=255)
 
     def __str__(self):
@@ -14,6 +23,7 @@ class Service(models.Model):
 class Info(models.Model):
     service = models.ForeignKey(Service, related_name='theService', on_delete=CASCADE)
     info = models.TextField()
+    order = models.IntegerField(default=1)
     def __str__(self):
         return f"Info for {self.service.title}"
 
